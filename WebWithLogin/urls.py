87271292364b2e991f1a81414021887b34a17cc1
York <2727965239@qq.com>
2024-11-07 19:46:47 +0800

    Create accounts app

--- WebWithLogin/urls.py
+++ WebWithLogin/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'WebWithLogin'
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:Item_id>/", views.detail, name="detail"),
--- WebWithLogin/views.py
+++ WebWithLogin/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import loader
 from .models import Item
 
@@ -7,11 +7,20 @@ from .models import Item
 
 def index(request):
     Item_recommended_list = Item.objects.filter(recommended=True)
-    template = loader.get_template("index.html")
+    template = loader.get_template("WebWithLogin/index.html")
     context = {
         "Item_recommended_list": Item_recommended_list,
     }
-    return HttpResponse(template.render(context, request))
+    # return HttpResponse(template.render(context, request))
+    return render(request, "WebWithLogin/index.html", context)
 
 def detail(request, Item_id):
-    return HttpResponse(f"detail item{Item_id}")
+    try:
+        item = Item.objects.get(pk=Item_id)
+    except item.DoesNotExist:
+        raise Http404("Item does not exist")
+    return render(request, "WebWithLogin/detail.html", {"item": item})
+
+    #another way
+    #item = get_object_or_404(Item, pk = Item_id)
+    #return render(request, "template_name", {})
